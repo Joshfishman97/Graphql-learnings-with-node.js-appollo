74120e2546ff1029cd6a52db8f23271ecf064cd1
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-20 18:34:42 +0000

    almost done with the basics of graphql

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require("apollo-server")
+
+const typeDefs = gql`
+type SkiDay{
+    id: ID!  
+    date: String!
+    mountain : String!
+    conditions: Conditions
+   }
+
+enum Conditions{
+    POWDER
+    HEAVY
+    ICE
+    THIN
+}
+    type Query{
+     totalDays: Int!  
+     allDays : [SkiDay!]!
+    }
+    input AddDayInput {
+        date: String!
+        mountain: String!
+        conditions: Conditions
+    }
+    type Mutation {
+            addDay(input: AddDayInput!): SkiDay
+        removeDay(id: ID!): SkiDay!
+    }
+`;
+
+    
+const server = new ApolloServer({
+    typeDefs,
+    mocks: true
+});
+
+server.listen().then(({url}) => console.log('Server running at ${url}'))
