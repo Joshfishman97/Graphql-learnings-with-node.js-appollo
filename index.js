4da2190ab7a62735408e7f2b5e0cf2f8803f905c
Joshfishman97 <fishmanjosh251@gmail.com>
2021-10-20 19:51:16 +0000

    figuring out the issue with the constructor and the mocklist

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer, gql, Mocklist } = require("apollo-server")
 
 const typeDefs = gql`
+    scalar Date
+
+
 type SkiDay{
     id: ID!  
-    date: String!
+    date: Date!
     mountain : String!
     conditions: Conditions
    }
 
+
+    type RemoveDayPayLoad {
+        day:SkiDay!
+        removed: Boolean
+        totalBefore: Int
+        totalAfter: Int
+    }
 enum Conditions{
     POWDER
     HEAVY
     ICE
     THIN
 }
-    type Query{
+
+    type Query {
      totalDays: Int!  
      allDays : [SkiDay!]!
     }
+
     input AddDayInput {
-        date: String!
+        date: Date!
         mountain: String!
         conditions: Conditions
     }
+    
     type Mutation {
             addDay(input: AddDayInput!): SkiDay
-        removeDay(id: ID!): SkiDay!
+        removeDay(id: ID!): RemoveDayPayLoad!
     }
+    type Subscription{
+        newDay: SkiDay!}
 `;
+
+const resolvers = {
+    Query: {
+      resolved: () => 'Resolved',
+    },
+  };
+
+const mocks = {
+    Date: () => "1/2/2025",
+    String: () => "Cool Data",
+    Query: () => ({
+        allDays: () => new Mocklist([1,15])
+    })
+};
 
     
 const server = new ApolloServer({
     typeDefs,
-    mocks: true
+    mocks,
+    Mocklist
 });
 
-server.listen().then(({url}) => console.log('Server running at ${url}'))
+server.
+listen()
+.then(({url}) => 
+console.log('Server running at ${url}'))
